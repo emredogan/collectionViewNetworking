e8a12af92dd6d6e76fdb41e0f0ac125a6a94f58c
Emre Dogan <emredogan@outlook.com>
2022-03-19 19:48:37 +0100

    Working copy

--- CollectionView_Networking/HeroCollectionViewCell.swift
+++ CollectionView_Networking/HeroCollectionViewCell.swift
@@ -9,4 +9,15 @@ import UIKit
 
 class HeroCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var heroImage: UIImageView!
+    @IBOutlet weak var heroName: UILabel!
+    
+    func setup(with hero: Hero) {
+        let urlString = "https://api.opendota.com"+hero.img
+        print("URLLL ", urlString)
+        let url = URL(string: urlString)!
+        heroImage.downloaded(from: url)
+        heroName.text = hero.localized_name
+    }
+    
 }
--- CollectionView_Networking/ViewController.swift
+++ CollectionView_Networking/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var heroes = [Hero]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        
+        downloadJson {
+            print("SUCCESS", self.heroes)
+            self.collectionView.reloadData()
+        }
+        
+    }
+    
+    func downloadJson(completed: @escaping () -> ()) {
+        let url = URL(string: "https://api.opendota.com/api/heroStats")
+        URLSession.shared.dataTask(with: url!) { data, url, error in
+            if (error == nil) {
+                do {
+                     self.heroes = try JSONDecoder().decode([Hero].self, from: data!)
+                    DispatchQueue.main.async {
+                        completed()
+                    }
+                    
+                } catch  {
+                    print("ERROR ", error)
+                }
+                
+            }
+        }.resume()
+        
     }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return heroes.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeroCollectionViewCell", for: indexPath) as! HeroCollectionViewCell
+        cell.setup(with: heroes[indexPath.row])
+        return cell
+    }
+
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 100)
+    }
 }
 
