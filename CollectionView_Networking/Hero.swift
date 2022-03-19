2e378d07adb7ba522fa309bc62b83af6c727cfff
Emre Dogan <emredogan@outlook.com>
2022-03-19 19:47:58 +0100

    first commit

---
+++ CollectionView_Networking/Hero.swift
@@ -0,0 +1,8 @@
+//
+//  Hero.swift
+//  CollectionView_Networking
+//
+//  Created by Emre Dogan on 18/03/2022.
+//
+
+import Foundation
---
+++ CollectionView_Networking/HeroCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HeroCollectionViewCell.swift
+//  CollectionView_Networking
+//
+//  Created by Emre Dogan on 18/03/2022.
+//
+
+import UIKit
+
+class HeroCollectionViewCell: UICollectionViewCell {
+    
+}
